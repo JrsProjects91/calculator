c005c3926a4cf23fb9947e2b9cf6b0faa3a1e4c6
JrsProjects91 <118298017+JrsProjects91@users.noreply.github.com>
2025-08-17 21:43:21 -0400

    further rework code,small  update to  html and css

--- script.js
+++ script.js
@@ -1,188 +1,120 @@
-const calcDisplay = document.querySelector('#display')
-const inputForm = document.querySelector('#input')
-const addButton = document.querySelector('#add')
-const subtractButton = document.querySelector('#subtract')
-const multiplyButton = document.querySelector('#multiply')
-const divideButton = document.querySelector('#divide')
-const equalButton = document.querySelector('#equal')
-const displayStringDOM = document.querySelector('#display-string')
-const numberButtons = document.querySelectorAll('.num')
-const clearButton = document.querySelector('#clear')
-
-let displayString = ""
-let obj = {
-    wasEqualPressed: false,
-    prevResult: 0,
-    operator: "",
-    prevOperator: ""
+let calc = {
+    firstNum: null,
+    secNum: null,
+    operator: null,
+    prevOperator: null,
+    isNum: false
 }
 
-function updateObj(obj,op,wasEq, value) {
-    const objLength = Object.keys(obj).length
-    obj.operator = op
-    obj.wasEqualPressed = wasEq
-    if (objLength === 4) {
-        if (wasEq) {
-            obj.prevOperator = op
-            return
-        }
-        obj.prevOperator = op
-        obj.firstNum = value
-        inputForm.value = ""
+const display = document.querySelector('#display');
+const inputForm = document.querySelector('#input');
+const numberButtons = document.querySelectorAll('.num');
+const clearButton = document.querySelector('#clear');
+const operators = document.querySelectorAll('#add, #subtract, #multiply, #divide, #equal');
 
-        
-    } else if (objLength === 5) {
-        if (wasEq && obj.prevOperator !== "=") {
-            obj.secNum = value
-            obj.prevResult = value
-        } 
-        else {
-                    obj.secNum = value
-        }
-
-        if (wasEq === true) {
-           console.log('operateee')
-           obj.firstNum = operate(op)
-            obj.secNum = value
-            obj.prevResult = value
-           obj.prevOperator = "="
-        } 
-        else {
-        if (obj.prevOperator === "=") {
-            console.log("hrerer")
-            obj.firstNum = operate(op)
-            delete obj.secNum
-            inputForm.value = ""
-            obj.prevOperator = op
-        } 
-        else {
-            obj.firstNum = operate(op)
-            delete obj.secNum
-            obj.prevOperator = op
-            inputForm.value = ""
-            } 
-        }
-    } else {
-        if (wasEq) {
-            console.log("here")
-            obj.secNum = obj.prevResult
-            obj.firstNum = operate(obj.operator)
-        } else {
-           console.log('operateee2')
-           delete obj.secNum
-           obj.prevOperator = "="
-           inputForm.value = ""
-        }
-
-    }
-}
-
-function operate(op) {
-        switch (op) {
-        case "+":
-            inputForm.value = add(obj.firstNum,obj.secNum)
-            return Number(obj.firstNum + obj.secNum)
-        case "-":
-            inputForm.value = subtract(obj.firstNum,obj.secNum) 
-            return obj.firstNum - obj.secNum
-        case "*":
-           inputForm.value = multiply(obj.firstNum,obj.secNum)
-            return obj.firstNum * obj.secNum
-        case "/":
-            inputForm.value = divide(obj.firstNum,obj.secNum)
-            return obj.firstNum / obj.secNum
-    }
-}
-
-function updateArray(value, op) {
-    value = Number(value)
-    if (displayArray.length < 1) {
-        displayArray.push(value)
-    } else if (displayArray.length == 1) {
-        displayArray.push(value)
-        equal(displayArray, op)
-        displayArray = [prevValue]
-    } else {
-    }
-    console.log(displayArray)
-}
-
-function returnInput() {
-    const val = Number(inputForm.value)
-    return val
-}
-
-
-numberButtons.forEach(number => {
-    number.addEventListener("click", (e) => {
-        inputForm.value += e.target.id
-    })
-})
-
-
-addButton.addEventListener("click", () => {
-    let value = returnInput()
-    updateObj(obj,'+',false,value)
-    console.log(obj)
-})
-
-subtractButton.addEventListener("click", () => {
-    let value = returnInput()
-    updateObj(obj,'-',false,value)
-    console.log(obj)
-
-})
-
-multiplyButton.addEventListener("click", () => {
-        let value = returnInput()
-    updateObj(obj,'*',false,value)
-    console.log(obj)
-
-})
-
-divideButton.addEventListener("click", () => {
-        let value = returnInput()
-    updateObj(obj,'/',false,value)
-    console.log(obj)
-
-})
-equalButton.addEventListener("click", () => {
-    let value = returnInput()
-    updateObj(obj,obj.operator,true,value)
-    console.log(obj)
-    
-})
-
-clearButton.addEventListener("click", () => {
-obj = {
-    wasEqualPressed: false,
-    prevResult: 0,
-    operator: "",
-    prevOperator: ""
-}
-inputForm.value = ""
-})
-
-function add(a,b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a,b) {
-    return a * b
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a,b) {
-    return a / b
+function divide(a, b) {
+    if (b === 0) {
+        return "Error Cant Divide By Zero";
+    }
+    return a / b;
 }
 
+function performCalculation(operator, firstOperand, secondOperand) {
+    switch (operator) {
+        case '+':
+            return add(firstOperand, secondOperand);
+        case '-':
+            return subtract(firstOperand, secondOperand);
+        case '*':
+            return multiply(firstOperand, secondOperand);
+        case '/':
+            return divide(firstOperand, secondOperand);
+        default:
+            return secondOperand;
+    }
+}
 
-/// store 1stnum , operator, secnum, prevresult, wasequal in obj
-// obj should have 3 keys after each equal (operator, prevresult and wasequal)
-// once we type out a nuber we only have 5 options, the operators or equal (as of now)
-// if we hit equal while the input screen is empty and while we only have the 1st number then nothing should happen
-// if we hit equal while we have something in input then we need to calculate 1stnum and secnm with the operator then remove the 2 num properties
-// if we hit one of the operators again after the 1st number we should calculate using that operator 
+function updateDisplay() {
+    inputForm.value = calculator.displayValue;
+}
+
+operators.forEach(button => {
+    button.addEventListener("click", () => {
+        operatorLogic(button.textContent)
+    })
+})
+
+function operatorLogic(buttonID) {
+    if (!calc.operator) {
+        calc.operator = buttonID
+    } else {
+        calc.prevOperator = calc.operator
+        calc.operator = buttonID
+    }
+    if (!calc.firstNum) {
+        calc.firstNum = Number(inputForm.value)
+        calc.isNum = false
+        if (calc.operator === "=") {
+            return
+        }
+        inputForm.value = ""
+    } else {
+        calc.secNum = Number(inputForm.value)
+        if (calc.operator === "=") {
+            calc.firstNum = performCalculation(calc.prevOperator, calc.firstNum,calc.secNum)
+            inputForm.value = calc.firstNum
+            calc.firstNum = null;
+            calc.secNum = null;
+            calc.operator = null;
+            calc.prevOperator = null; 
+            calc.isNum = true;
+        } else if (calc.operator === calc.prevOperator) {
+             if (!calc.isNum) {
+                calc.firstNum = performCalculation(calc.operator, calc.firstNum,calc.firstNum)
+             } else {
+            calc.firstNum = performCalculation(calc.operator, calc.firstNum,calc.secNum)
+             }
+            inputForm.value = calc.firstNum
+            calc.isNum = false
+        } else {
+            calc.firstNum = performCalculation(calc.prevOperator, calc.firstNum,calc.secNum)
+            inputForm.value = calc.firstNum
+            calc.isNum = false;
+        }
+    }
+    console.log(calc)
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (!calc.isNum) {
+          inputForm.value = button.textContent;
+          calc.isNum = true;
+        } else {
+            inputForm.value += button.textContent;
+            calc.isNum = true;
+        }
+
+    })
+})
+
+clearButton.addEventListener("click", () => {
+    calc.firstNum = null;
+    calc.secNum = null;
+    calc.operator = null;
+    calc.prevOperator = null;
+    calc.isNum = false;
+    inputForm.value = ""
+})
